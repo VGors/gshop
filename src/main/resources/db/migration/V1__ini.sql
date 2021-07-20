bc4fff1971e418d8b3cb10fbec70f628173ae235
Victor <v.gorshenev@gmail.com>
2021-07-20 18:40:51 +0500

    flyway experiments

--- src/main/resources/db/migration/V1__ini.sql
+++ src/main/resources/db/migration/V1__ini.sql
@@ -4,8 +4,3 @@ create table products
     title varchar(255),
     price float
 );
-insert into products (title, price)
-values ('Celery', 50.32),
-       ('Parsley', 30.11),
-       ('Fennel', 10),
-       ('Coriander', 46.7);
---
+++ src/main/resources/db/migration/V2__ini.sql
@@ -0,0 +1,5 @@
+insert into products (title, price)
+values ('Celery', 50.32),
+       ('Parsley', 30.11),
+       ('Fennel', 10),
+       ('Coriander', 46.7);
