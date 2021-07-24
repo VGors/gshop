b5a4899531a428c1abfd0c4754d78daa22f28445
Victor <v.gorshenev@gmail.com>
2021-07-24 17:46:30 +0500

    final HW

--- src/main/java/com/gorvic/gshop/controllers/ProductController.java
+++ src/main/java/com/gorvic/gshop/controllers/ProductController.java
@@ -32,13 +32,11 @@ public class ProductController {
     }
 
     @GetMapping("/products/{id}")
-    @ResponseBody
     public Product showProductInfo(@PathVariable Long id) {
         return productService.findById(id);
     }
 
     @GetMapping("/products/delete/{id}")
-//    @ResponseBody
     public String deleteProductById(@PathVariable Long id) {
         productService.deleteById(id);
         return "redirect:/";
@@ -46,21 +44,18 @@ public class ProductController {
 
     //    http://localhost:8189/gshop/products/more_min_price?min=800
     @GetMapping("/products/more_min_price")
-    @ResponseBody
     public List<Product> findMoreMinPrice(@RequestParam(name = "min") float min) {
         return productService.findMoreMinPrice(min);
     }
 
     //    http://localhost:8189/gshop/products/less_max_price?max=100
     @GetMapping("/products/less_max_price")
-    @ResponseBody
     public List<Product> findLessMaxPrice(@RequestParam(name = "max") float max) {
         return productService.findLessMaxPrice(max);
     }
 
     //    http://localhost:8189/gshop/products/between_min_max?min=40&max=780
     @GetMapping("/products/between_min_max")
-    @ResponseBody
     public List<Product> findBetweenMinMaxPrice(@RequestParam(name = "min") float min, @RequestParam(name = "max") float max) {
         return productService.findBetweenMinMaxPrice(min, max);
     }
